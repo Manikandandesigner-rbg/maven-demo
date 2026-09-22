e1b4fef70f139f86286e7a7d5fbc6fcf1d225f6c
Manikandan <manikandan.ceg12@gmail.com>
2026-09-22 17:07:13 +0530

    Create HelloWorldTest.java

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloWorldTest {
+
+    @Test
+    void testAddition() {
+        int result = 10 + 20;
+
+        assertEquals(30, result);
+    }
+}
